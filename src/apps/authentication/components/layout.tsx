3d9e619f413d0e60954d805c69d72b39175ece02
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-02-13 10:27:25 +0100

    feat: add authentication layout redirect for logged-in users

--- src/apps/authentication/components/layout.tsx
+++ src/apps/authentication/components/layout.tsx
@@ -1,6 +1,19 @@
-import { Outlet } from 'react-router'
+import { getUserState } from '@/data/store/user_store'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet, useNavigate } from 'react-router'
 
-export default function AuthenticationLayout() {  return (
+export default function AuthenticationLayout() {  
+  const { user } = useSelector(getUserState)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (user) {
+      navigate('/archery/dashboard')
+    }
+  }, [user])
+
+  return (
     <div>
       Auth Layout
       <Outlet/>
