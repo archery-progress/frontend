9e471aeb9faec7c888b04972fd725c60b7192b23
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-02-13 10:27:33 +0100

    refactor: improve Redux store configuration and user state management

--- src/data/store/store.ts
+++ src/data/store/store.ts
@@ -1,25 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { userSlice } from '@/data/store/user_store'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { userReducer } from '@/data/store/user_store'
 import { memberApi } from '@/data/api/member_api.ts'
 import { roleApi } from '@/data/api/role_api.ts'
 import { structureApi } from '@/data/api/structure_api.ts'
 import { authApi } from '@/data/api/auth_api.ts'
 
-export const store = configureStore({
-  reducer: {
-    user: userSlice.reducer,
-    [authApi.reducerPath]: authApi.reducer,
-    [memberApi.reducerPath]: memberApi.reducer,
-    [roleApi.reducerPath]: roleApi.reducer,
-    [structureApi.reducerPath]: structureApi.reducer,
-  },
-  middleware: (middleware) =>
-    middleware()
-      .concat(authApi.middleware)
-      .concat(memberApi.middleware)
-      .concat(roleApi.middleware)
-      .concat(structureApi.middleware),
+export const rootReducer = combineReducers({
+  user: userReducer,
+  [authApi.reducerPath]: authApi.reducer,
+  [memberApi.reducerPath]: memberApi.reducer,
+  [roleApi.reducerPath]: roleApi.reducer,
+  [structureApi.reducerPath]: structureApi.reducer,
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export function setupStore(preloadedState?: never) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (middleware) =>
+      middleware()
+        .concat(authApi.middleware)
+        .concat(memberApi.middleware)
+        .concat(roleApi.middleware)
+        .concat(structureApi.middleware),
+  })
+
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
