5de46903435f3bee3142d7a4bef30575fb405fd8
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-02-13 10:28:09 +0100

    refactor: centralize routing and authentication logic in App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
-import { store } from './data/store/store'
-import LoginPage from './apps/authentication/pages/login_page'
+import { setupStore } from './data/store/store'
 import '@/commons/assets/css/app.css'
-import AuthenticationLayout from '@/apps/authentication/components/layout'
-import AuthenticatedLayout from '@/commons/components/layouts/authenticated_layout'
-import { Toaster } from '@/commons/components/ui/sonner'
-import { ArcheryDashboard } from '@/apps/archery/pages/archery_dashboard'
-import { PlatformDashboard } from '@/apps/platform/pages/platform_dashboard'
-import { MembersOverview } from '@/apps/platform/pages/members/members_overview.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import App from './apps/app'
+
+const container = document.getElementById('root') || document.createElement('div') as HTMLElement
+const root = createRoot(container)
+
+const store = setupStore()
+
+const render = (
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path="authentication" element={<AuthenticationLayout/>}>
-            <Route path="login" element={<LoginPage/>}/>
-          </Route>
-          <Route path="archery" element={<AuthenticatedLayout />}>
-            <Route path="dashboard" element={<ArcheryDashboard/>}/>
-          </Route>
-          <Route path="structures/:structureId" element={<AuthenticatedLayout />}>
-            <Route path="overview" element={<PlatformDashboard/>}/>
-            <Route path="members/overview" element={<MembersOverview/>} />
-            <Route path="members/:memberId/view" element={<MembersOverview/>} />
-          </Route>
-        </Routes>
+        <App />
       </BrowserRouter>
     </Provider>
-    <Toaster />
   </StrictMode>
 )
+
+root.render(render)
