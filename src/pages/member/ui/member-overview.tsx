11f9cbc7c468e9d44958a9c77b1c6a0b28c1b93e
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-03-02 13:29:49 +0100

    feat: add breadcrumb navigation for member overview page

--- src/pages/member/ui/member-overview.tsx
+++ src/pages/member/ui/member-overview.tsx
@@ -3,6 +3,7 @@ import { Member } from '@/data/models/user.ts'
 import { Badge } from '@/commons/components/ui/badge.tsx'
 
 import { Params } from 'react-router'
+import { useDefineBreadcrumb } from '@/commons/components/layouts/default/hooks'
 
 export interface  MemberOverviewProps {
   member: Member
@@ -10,6 +11,17 @@ export interface  MemberOverviewProps {
 }
 
 export default function MemberOverview({ params, member }: MemberOverviewProps) {
+  useDefineBreadcrumb([
+    {
+      label: 'Licenciés',
+      url: `/structures/${params.structureId}/members/overview`,
+    },
+    {
+      label: `${member.user.firstname} ${member.user.lastname}`,
+      url: `/structures/${params.structureId}/members/${member.id}/overview`,
+    },
+  ])
+  
   return (
     <div>
       <div className="p-5 border-b">
