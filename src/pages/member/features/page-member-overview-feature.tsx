280ed5a6ad07ddbd6cdadc5695c9f7707fc8d438
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-03-02 13:28:09 +0100

    refactor: remove ApplicationLayout from member overview pages

--- src/pages/member/features/page-member-overview-feature.tsx
+++ src/pages/member/features/page-member-overview-feature.tsx
@@ -2,7 +2,6 @@ import { useParams } from 'react-router'
 import { useGetMemberQuery } from '@/data/api/member_api.ts'
 import { useEffect } from 'react'
 import { Skeleton } from '@/commons/components/ui/skeleton'
-import { ApplicationLayout } from '@/commons/components/layouts/default/layout'
 import MemberOverview from '../ui/member-overview'
 
 export default function PageMemberViewFeature() {
@@ -35,10 +34,8 @@ export default function PageMemberViewFeature() {
 
 function MemberLoading() {
   return (
-    <ApplicationLayout mode="platform">
-      <div className="p-5 border-b">
-        <Skeleton className="w-full h-10" />
-      </div>
-    </ApplicationLayout>
+    <div className="p-5 border-b">
+      <Skeleton className="w-full h-10" />
+    </div>
   )
 }
--- src/pages/member/ui/member-overview.tsx
+++ src/pages/member/ui/member-overview.tsx
@@ -1,37 +1,28 @@
 
 import { Member } from '@/data/models/user.ts'
 import { Badge } from '@/commons/components/ui/badge.tsx'
-import { ApplicationLayout } from '@/commons/components/layouts/default/layout.tsx'
 
 import { Params } from 'react-router'
 
-type Props = {
+export interface  MemberOverviewProps {
   member: Member
   params: Readonly<Params<string>>
 }
 
-export default function MemberOverview({ params, member }: Props) {
+export default function MemberOverview({ params, member }: MemberOverviewProps) {
   return (
-    <ApplicationLayout
-      mode="platform"
-      breadcrumb={[
-        {label: 'Accueil', url: `/platform/${params.structureId}/overview`},
-        {label: 'Licenciés', url: `/platform/${params.structureId}/members/overview`},
-      ]}
-    >
-      <div>
-        <div className="p-5 border-b">
-          <div>
-            <h2 className='className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0"'>
-              <span>{member.user.firstname} {member.user.lastname}</span>
-            </h2>
+    <div>
+      <div className="p-5 border-b">
+        <div>
+          <h2 className='className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0"'>
+            <span>{member.user.firstname} {member.user.lastname}</span>
+          </h2>
 
-            <span>
-              <Badge variant="outline">{member.structure.name}</Badge>
-            </span>
-          </div>
+          <span>
+            <Badge variant="outline">{member.structure.name}</Badge>
+          </span>
         </div>
       </div>
-    </ApplicationLayout>
+    </div>
   )
 }
